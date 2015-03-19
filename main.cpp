d03a0897924496d163104f4c7dce3c58ca12532d
Ricardo <none@no_server.com>
2015-03-19 22:09:28 +0000

    Move enums and Node/NodeState defs to a header file

--- main.cpp
+++ main.cpp
@@ -15,30 +15,17 @@
 // debugging
 #include <cassert>
 
+// project files
+#include "node.h"
+
 int __debug__ = 1; /* toggle for debug prints*/
-
-#define DEBUG_LOG(...)                  \
-    do {                                \
-        if (__debug__  != 0) {          \
-            printf(__VA_ARGS__);        \
-            printf("\n");               \
-        }                               \
-    } while (0)
-
-
-enum
-{
-    // we could *mostly* make it work with a 15 puzzle by editing this
-    MATRIX_SIDE_SIZE    = 3,
-    MATRIX_SIZE         = MATRIX_SIDE_SIZE * MATRIX_SIDE_SIZE,
-};
 
 /// global vars
 int init[MATRIX_SIZE] = {
 // 24 steps
-//	3, 4, 2,
-//	5, 1, 7,
-//	6, 0, 8,
+	3, 4, 2,
+	5, 1, 7,
+	6, 0, 8,
 
 // test, 3 steps
 //	1, 2, 3,
@@ -46,9 +33,9 @@ int init[MATRIX_SIZE] = {
 //	7, 0, 6,
 
 // test, 6 steps
-    0, 2, 3,
-    1, 8, 5,
-    7, 4, 6,
+//    0, 2, 3,
+//    1, 8, 5,
+//    7, 4, 6,
 };
 
 int goal[MATRIX_SIZE] = {
@@ -57,168 +44,11 @@ int goal[MATRIX_SIZE] = {
 	7, 6, 5,
 };
 
-#define MATRIX_STRUCT_SIZE sizeof(init)
-
 // just for measuring
 int node_counter = 0;
 
-struct Node;
-
 std::list<Node*> nodeQueue;
 Node* root = nullptr;
-
-enum SearchType
-{
-    SEARCH_TYPE_BFS,
-    SEARCH_TYPE_DFS,
-    SEARCH_TYPE_IDFS,
-    SEARCH_TYPE_GREEDY,
-    SEARCH_TYPE_A_STAR,
-};
-
-void FillFirstNode();
-void AddToQueueBFS(std::list<Node*> descList);
-void AddToQueueDFS(std::list<Node*> descList);
-void AddToQueueIDFS(std::list<Node*> descList);
-void AddToQueueGreedy(std::list<Node*> descList);
-void AddToQueueAStar(std::list<Node*> descList);
-
-void Finish(Node* node);
-
-void GeneralSearchAlgorithm(SearchType searchType);
-
-// arg parsing helper
-bool stringEndsWith(std::string s, std::string end);
-
-struct Node
-{
-	Node()
-	{
-        node_counter_n = node_counter;
-        
-        ++node_counter;
-        if (node_counter % 1000 == 0)
-            DEBUG_LOG("Node::Node - allocated %d nodes", node_counter);
-        
-		memset(state, 0, MATRIX_STRUCT_SIZE);
-		parent = nullptr;
-		move = 0;
-		depth = 0;
-		cost = 0;
-	}
-
-	int state[MATRIX_SIZE];
-	Node* parent;
-	int move; // applied move, the number that was switched with '0'
-	int depth; // calculated from parent
-	int cost; // effective cost since root, calculated from parent
-    
-    int node_counter_n;
-
-	bool IsGoal()
-	{
-		return memcmp(state, goal, MATRIX_STRUCT_SIZE) == 0;
-	}
-
-    std::list<Node*> MakeDescendants()
-    {
-        std::list<Node*> list;
-
-        static const std::vector<std::vector<int>> offsets = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
-        
-        for (int idx = 0; idx < MATRIX_SIZE; ++idx)
-        {
-            if (state[idx] != 0)
-                continue;
-            
-            int i, j;
-            IdxToCoords(i, j, idx);
-            
-            for (std::vector<int> offset : offsets)
-            {
-#define IS_VALID_COORDS(x, y) (x >= 0 && x < MATRIX_SIDE_SIZE && y >= 0 && y < MATRIX_SIDE_SIZE)
-
-                if (!IS_VALID_COORDS(i + offset[0], j + offset[1]))
-                    continue;
-                
-                int newIdx = CoordsToIdx(i + offset[0], j + offset[1]);
-                
-                Node* node = new Node();
-                node->parent = this;
-                node->depth = depth + 1;
-                memcpy(node->state, state, MATRIX_STRUCT_SIZE);
-                
-                node->DoMove(idx, newIdx); // must be after state initialization
-                
-                list.push_back(node);
-            }
-        }
-
-        return list;
-    }
-
-    // used heuristic
-    int GetManhattanDist() const
-    {
-        int result = 0;
-        for (int idx = 0; idx < MATRIX_SIZE; ++idx)
-        {
-            if (state[idx] == 0)
-                continue;
-
-            // find the equivalent in the goal matrix
-            for (int k = 0; k < MATRIX_SIZE; ++k)
-            {
-                if (state[idx] == goal[k])
-                {
-                    // get our x/y coordinates
-                    int i, j;
-                    IdxToCoords(i, j, idx);
-                    
-                    // get the goal's x/y coordinates
-                    int x, y;
-                    IdxToCoords(x, y, k);
-                
-                    result += std::abs(i - x) + std::abs(j - y);
-                    break;
-                }
-            }
-        }
-        
-        return result;
-    }
-    
-    int CoordsToIdx(int i, int j) const { return i + j * MATRIX_SIDE_SIZE; }
-    
-    void IdxToCoords(int& i, int& j, int idx) const
-    {
-        i = idx % MATRIX_SIDE_SIZE;
-        j = idx / MATRIX_SIDE_SIZE;
-    }
-    
-private:
-    // pretty much a swap between index a and b
-	void DoMove(int a, int b)
-	{
-        assert(state[a] == 0 && a != b);
-        
-		move = state[a] = state[b];
-		state[b] = 0;
-        
-        cost = depth + GetManhattanDist();
-        
-        if (node_counter % 1000 == 0)
-        {
-            for (int i = 0; i < MATRIX_SIZE; ++i)
-                printf("%d ", state[i]);
-            
-            printf("\n");
-            printf("Cost is %d, depth is %d\n", cost, depth);
-            printf("Cost of the front of the queue is %d, depth is %d\n",
-                nodeQueue.front()->cost, nodeQueue.front()->depth);
-        }
-	}
-};
 
 int main(int argc, char** argv)
 {
@@ -236,17 +66,17 @@ int main(int argc, char** argv)
         }
         else if (strcmp(argv[i], "--search_type") == 0)
         {
-            if (stringEndsWith(argv[i], "bfs"))
+            if (StringEndsWith(argv[i], "bfs"))
                 ;
-            else if (stringEndsWith(argv[i], "bfs"))
+            else if (StringEndsWith(argv[i], "bfs"))
                 ;
-            else if (stringEndsWith(argv[i], "dfs"))
+            else if (StringEndsWith(argv[i], "dfs"))
                 ;
-            else if (stringEndsWith(argv[i], "idfs"))
+            else if (StringEndsWith(argv[i], "idfs"))
                 ;
-            else if (stringEndsWith(argv[i], "greedy"))
+            else if (StringEndsWith(argv[i], "greedy"))
                 ;
-            else if (stringEndsWith(argv[i], "astar"))
+            else if (StringEndsWith(argv[i], "astar"))
                 ;
             else
             {
@@ -276,8 +106,8 @@ void FillFirstNode()
 {
 	Node* node = new Node();
 	// init states
-	memcpy(node->state, init, MATRIX_STRUCT_SIZE);
-
+    node->_state = init;
+    
 	root = node;
 
 	nodeQueue.push_back(node);
@@ -304,7 +134,7 @@ void AddToQueueIDFS(std::list<Node*> descList)
         
 	for (Node* node : descList)
     {
-        if (node->depth <= IDFS_depth)
+        if (node->_depth <= IDFS_depth)
             nodeQueue.push_front(node);
         
         nodesToDelete.push_back(node);
@@ -349,20 +179,6 @@ void AddToQueueGreedy(std::list<Node*> descList)
 		nodeQueue.insert(itr, node);
     }
 }
-struct NodeState
-{
-    NodeState(int s[MATRIX_SIZE])
-    {
-        memcpy(state, s, MATRIX_STRUCT_SIZE);
-    }
-    
-    int state[MATRIX_SIZE];
-    
-    bool operator== (NodeState const& node) const { return memcmp(state, node.state, MATRIX_STRUCT_SIZE) == 0; }
-   
-    bool operator!= (NodeState const& node) const { return memcmp(state, node.state, MATRIX_STRUCT_SIZE) != 0; }
-    bool operator< (NodeState const& node) const { return false; }
-};
 
 // needed to be able to use std::unordered_set<NodeState> (a hash map)
 namespace std
@@ -372,15 +188,11 @@ namespace std
     {
         std::size_t operator()(const NodeState& nodeState) const
         {
-            using std::size_t;
-            using std::hash;
-            using std::string;
-
-            hash<int> hasher;
+            std::hash<int> hasher;
       
             std::size_t h = 0;
             for (int i = 0; i < MATRIX_SIZE; ++i)
-                h = h * 31 + hasher(nodeState.state[i]);
+                h = h * 31 + hasher(nodeState._state[i]);
       
             return h;
         }
@@ -393,12 +205,12 @@ void AddToQueueAStar(std::list<Node*> descList)
     
 	for (Node* node : descList)
     {
-        NodeState state = node->state;
+        NodeState& state = node->_state;
         if (visited.find(state) != visited.end())
             continue;
         
         visited.insert(state);
-        int cost = node->depth + node->GetManhattanDist();
+        int cost = node->_depth + node->GetManhattanDist();
         
         // manually *sort* the list
         // search through the list up to the point where nodes have a higher cost, and insert our node right before
@@ -407,7 +219,7 @@ void AddToQueueAStar(std::list<Node*> descList)
         for (; itr != nodeQueue.end(); ++itr)
         {
             Node* n = *itr;
-            if (cost <= n->depth + n->GetManhattanDist())
+            if (cost <= n->_depth + n->GetManhattanDist())
                 break;
         }
 
@@ -418,10 +230,10 @@ void AddToQueueAStar(std::list<Node*> descList)
 void Finish(Node* node)
 {
 	std::list<int> moveList;
-	while (node->parent)
+	while (node->_parent)
 	{
-		moveList.push_front(node->move);
-		node = node->parent;
+		moveList.push_front(node->_move);
+		node = node->_parent;
 	}
 
 	for (int i : moveList)
@@ -501,10 +313,114 @@ void GeneralSearchAlgorithm(SearchType searchType)
 */
 
 // arg parsing helper
-bool stringEndsWith(std::string s, std::string end) {
+bool StringEndsWith(std::string s, std::string end) {
     if (s.length() < end.length())
         return false;
     
     return s.compare(s.length() - end.length(), end.length(), end) == 0;
 }
+
+Node::Node()
+{
+    ++node_counter;
+    if (node_counter % 1000 == 0)
+        DEBUG_LOG("Node::Node - allocated %d nodes", node_counter);
+
+    _parent = nullptr;
+    _move = 0;
+    _depth = 0;
+}
+
+bool Node::IsGoal()
+{
+    return _state == goal;
+}
+
+std::list<Node*> Node::MakeDescendants()
+{
+    std::list<Node*> list;
+
+    static const std::vector<std::vector<int>> offsets = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+    
+    for (int idx = 0; idx < MATRIX_SIZE; ++idx)
+    {
+        if (_state._state[idx] != 0)
+            continue;
+        
+        int i, j;
+        IdxToCoords(i, j, idx);
+        
+        for (std::vector<int> offset : offsets)
+        {
+#define IS_VALID_COORDS(x, y) (x >= 0 && x < MATRIX_SIDE_SIZE && y >= 0 && y < MATRIX_SIDE_SIZE)
+
+            if (!IS_VALID_COORDS(i + offset[0], j + offset[1]))
+                continue;
+            
+            int newIdx = CoordsToIdx(i + offset[0], j + offset[1]);
+            
+            Node* node = new Node();
+            node->_parent = this;
+            node->_depth = _depth + 1;
+            node->_state = _state;
+            
+            node->DoMove(idx, newIdx); // must be after state initialization
+            
+            list.push_back(node);
+        }
+    }
+
+    return list;
+}
+
+int Node::GetManhattanDist() const
+{
+    int result = 0;
+    for (int idx = 0; idx < MATRIX_SIZE; ++idx)
+    {
+        if (_state._state[idx] == 0)
+            continue;
+
+        // find the equivalent in the goal matrix
+        for (int k = 0; k < MATRIX_SIZE; ++k)
+        {
+            if (_state._state[idx] == goal[k])
+            {
+                // get our x/y coordinates
+                int i, j;
+                IdxToCoords(i, j, idx);
+                
+                // get the goal's x/y coordinates
+                int x, y;
+                IdxToCoords(x, y, k);
+            
+                result += std::abs(i - x) + std::abs(j - y);
+                break;
+            }
+        }
+    }
+    
+    return result;
+}
+
+int Node::CoordsToIdx(int i, int j) const
+{
+    return i + j * MATRIX_SIDE_SIZE;
+}
+
+void Node::IdxToCoords(int& i, int& j, int idx) const
+{
+    i = idx % MATRIX_SIDE_SIZE;
+    j = idx / MATRIX_SIDE_SIZE;
+}
+
+void Node::DoMove(int a, int b)
+{
+    assert(_state._state[a] == 0 && a != b);
+    
+    _move = _state._state[a] = _state._state[b];
+    _state._state[b] = 0;
+}
+
+
 
---
+++ node.h
@@ -0,0 +1,92 @@
+
+
+#ifndef _NODE_H_
+#define _NODE_H_
+
+extern int __debug__; /* toggle for debug prints*/
+
+#define DEBUG_LOG(...)                  \
+    do {                                \
+        if (__debug__  != 0) {          \
+            printf(__VA_ARGS__);        \
+            printf("\n");               \
+        }                               \
+    } while (0)
+
+enum SearchType
+{
+    SEARCH_TYPE_BFS,
+    SEARCH_TYPE_DFS,
+    SEARCH_TYPE_IDFS,
+    SEARCH_TYPE_GREEDY,
+    SEARCH_TYPE_A_STAR,
+};
+
+enum
+{
+    // we could *mostly* make it work with a 15 puzzle by editing this
+    MATRIX_SIDE_SIZE    = 3,
+    MATRIX_SIZE         = MATRIX_SIDE_SIZE * MATRIX_SIDE_SIZE,
+};
+
+#define MATRIX_STRUCT_SIZE (sizeof(int) * MATRIX_SIZE)
+
+struct NodeState
+{
+    NodeState() { memset(_state, 0, MATRIX_STRUCT_SIZE); }
+    NodeState(int state[MATRIX_SIZE])
+    {
+        memcpy(_state, state, MATRIX_STRUCT_SIZE);
+    }
+    
+    int _state[MATRIX_SIZE];
+    
+    bool operator== (NodeState const& node) const { return memcmp(_state, node._state, MATRIX_STRUCT_SIZE) == 0; }
+    bool operator== (int array[MATRIX_SIZE]) const { return memcmp(_state, array, MATRIX_STRUCT_SIZE) == 0; }
+   
+    bool operator!= (NodeState const& node) const { return memcmp(_state, node._state, MATRIX_STRUCT_SIZE) != 0; }
+    bool operator< (NodeState const& node) const { return false; }
+};
+
+struct Node
+{
+public:
+	Node();
+
+	NodeState _state;
+	Node* _parent;
+    
+	int _move; // applied move, the number that was switched with '0'
+	int _depth; // calculated from parent
+    
+public:
+	bool IsGoal();
+
+    std::list<Node*> MakeDescendants();
+
+    // used heuristic
+    int GetManhattanDist() const;
+    
+    int CoordsToIdx(int i, int j) const;
+    void IdxToCoords(int& i, int& j, int idx) const;
+    
+private:
+    // pretty much a swap between index a and b
+	void DoMove(int a, int b);
+};
+
+void FillFirstNode();
+void AddToQueueBFS(std::list<Node*> descList);
+void AddToQueueDFS(std::list<Node*> descList);
+void AddToQueueIDFS(std::list<Node*> descList);
+void AddToQueueGreedy(std::list<Node*> descList);
+void AddToQueueAStar(std::list<Node*> descList);
+
+void Finish(Node* node);
+
+void GeneralSearchAlgorithm(SearchType searchType);
+
+// arg parsing helper
+bool StringEndsWith(std::string s, std::string end);
+
+#endif // _NODE_H_
